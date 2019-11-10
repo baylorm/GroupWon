4ff67ae964ecb53dc0590560e8953dacb213e0be
kestranick <stranick@lafayette.edu>
2019-11-10 15:34:20 -0500

    Adding Person and Phone

--- people/models.py
+++ people/models.py
@@ -4,3 +4,14 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     name = models.CharField(max_length=150)
+
+
+class Person(models.Model):
+    first = models.CharField(max_length=20)
+    last = models.CharField(max_length=30)
+    email = models.emailField()
+    role = models.CharField(max_length=100)
+
+
+class Phone(models.Model):
+    number = models.IntegerField()
