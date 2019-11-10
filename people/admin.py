3aea9712eadbf3e0a7d48d1f4c98d5e041c51b00
Michael Baylor <baylor.michael@gmail.com>
2019-11-10 15:52:47 -0500

    Admin for person

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Person
+
 
 # Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first', 'last', 'email', 'role')
+    fields = ('first', 'last', 'email', 'role',)
+    search_fields = ('first', 'last', 'email')
+
+
+admin.site.register(Person, PersonAdmin)
