4166d178a436c3b484577d1b07e5866e94e5e5c0
bordenz <bordenz@lafayette.edu>
2019-11-10 15:38:03 -0500

    Created a model in outreach

--- outreach/models.py
+++ outreach/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    status = models.CharField(max_length=150)
+    notes = models.TextField()
+
