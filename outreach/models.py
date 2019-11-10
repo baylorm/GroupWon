4120a0bcf23048b82dfe0b17c14455bcb92d3d08
bordenz <bordenz@lafayette.edu>
2019-11-10 16:08:51 -0500

    Added an admin field for Organization and also implemented Foreign Keys

--- outreach/models.py
+++ outreach/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 
 # Create your models here.
-class Project(models.Model):
-    name = models.CharField(max_length=150)
-    status = models.CharField(max_length=150)
-    notes = models.TextField()
-
-
 class Organization(models.Model):
     name = models.CharField(max_length=150)
     type = models.CharField(max_length=150)
     num_members = models.IntegerField()
     notes = models.TextField()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    status = models.CharField(max_length=150)
+    notes = models.TextField()
+    community_partner = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True, related_name='community')
+    lafayette_organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True, related_name='lafayette')
 
 
 class Event(models.Model):
