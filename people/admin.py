d56d5cc7e35bb9688f3e9a0a99dc1193b8598ead
kestranick <stranick@lafayette.edu>
2019-11-10 16:01:50 -0500

    Added people admin classes

--- people/admin.py
+++ people/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Person
+from .models import Department
+from .models import Phone
+from .models import FacultyType
 
 
 # Register your models here.
@@ -9,4 +12,25 @@ class PersonAdmin(admin.ModelAdmin):
     search_fields = ('first', 'last', 'email')
 
 
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name',)
+    search_fields = ('name',)
+
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('number',)
+    fields = ('number',)
+    search_fields = ('number',)
+
+
+class FacultyTypeAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+    fields = ('type',)
+    search_fields = ('type',)
+
+
 admin.site.register(Person, PersonAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(FacultyType, FacultyTypeAdmin)
