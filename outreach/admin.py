96df894b53668f05528bf1556cd5fddb9e3fc57d
bordenz <bordenz@lafayette.edu>
2019-11-10 15:57:22 -0500

    Added an admin field for Project

--- outreach/admin.py
+++ outreach/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project
+
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status')
+    fields = ('name', 'status', 'notes',)
+    search_fields = ('name', 'status', 'notes')
+
+
+admin.site.register(Project, ProjectAdmin)
