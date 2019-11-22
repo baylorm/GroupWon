e8f99b1fb069515fde8c7f073fcc482a232ab52f
bordenz <bordenz@lafayette.edu>
2019-11-22 16:23:38 -0500

    Updated outreach's admin.py and models.py

--- outreach/admin.py
+++ outreach/admin.py
@@ -5,14 +5,21 @@ from .models import Project, Organization
 # Register your models here.
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'num_members')
-    fields = ('name', 'type', 'num_members', 'notes')
+    fields = ('name', 'type', 'num_members', 'notes', 'contact')
     search_fields = ('name', 'type', 'num_members', 'notes')
 
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'community_partner', 'lafayette_organization')
-    fields = ('name', 'status', 'notes', 'community_partner', 'lafayette_organization')
+    fields = ('name', 'status', 'notes', 'community_partner', 'lafayette_organization', 'student_partner',
+              'faculty_partner')
     search_fields = ('name', 'status', 'notes', 'community_partner', 'lafayette_organization')
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'pulse_date', 'num_volunteers')
+    fields = ('name', 'date', 'pulse_date', 'num_volunteers', 'notes', 'coordinator', 'contact', 'organization')
+    search_fields = ('name', 'date', 'pulse_date', 'num_volunteers', 'notes')
 
 
 admin.site.register(Organization, OrganizationAdmin)
--- outreach/models.py
+++ outreach/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+from people.models import Person
+
+
 class Organization(models.Model):
     name = models.CharField(max_length=150)
     type = models.CharField(max_length=150)
     num_members = models.IntegerField()
     notes = models.TextField()
+    contact = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -17,8 +21,14 @@ class Project(models.Model):
     status = models.CharField(max_length=150, choices=(('Upcoming', 'Upcoming'), ('Ongoing', 'Ongoing'),
                                                        ('Completed', 'Completed')))
     notes = models.TextField()
-    community_partner = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True, related_name='community')
-    lafayette_organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True, related_name='lafayette')
+    community_partner = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True,
+                                          related_name='community')
+    lafayette_organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True,
+                                               related_name='lafayette')
+    student_partner = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True,
+                                        related_name='student')
+    faculty_partner = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True,
+                                        related_name='faculty')
 
     def __str__(self):
         return self.name
@@ -30,6 +40,11 @@ class Event(models.Model):
     pulse_date = models.DateField()
     num_volunteers = models.IntegerField()
     notes = models.TextField()
+    coordinator = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True,
+                                    related_name='coordinator')
+    contact = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True,
+                                related_name='contact')
+    organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.name
