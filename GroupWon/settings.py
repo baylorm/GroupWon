baa1544567c694feb5fc92bb99b0b77d6247eb81
Michael Baylor <baylor.michael@gmail.com>
2019-11-24 17:26:02 -0500

    Added psql support

--- GroupWon/settings.py
+++ GroupWon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bl)lvy2d2a$wkz1%jr4#=gk6x4k1o25+3w@4*glq$tuz)226*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'GroupWon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'baylorm',
+            'USER': 'baylorm',
+            'PASSWORD': 'EnRajip8',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
