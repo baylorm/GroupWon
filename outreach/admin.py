c1684a5e98e5b2326e6db859c8919b57fd51492d
pantanor <pantanor@lafayette.edu>
2019-11-24 17:28:07 -0500

    filters

--- outreach/admin.py
+++ outreach/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Project, Organization, Event
 
+def mark_upcoming(modeladmin, request, queryset):
+    queryset.update(status='Upcoming')
+def mark_ongoing(modeladmin, request, queryset):
+    queryset.update(status='Ongoing')
+def mark_completed(modeladmin, request, queryset):
+    queryset.update(status='Completed')
 
 # Register your models here.
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'contact', 'num_members')
+    list_filter = ('type')
     fields = ('name', 'type', 'contact', 'num_members', 'notes')
     search_fields = ('name', 'type', 'contact', 'num_members', 'notes')
 
@@ -12,14 +19,17 @@ class OrganizationAdmin(admin.ModelAdmin):
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'student_partner', 'faculty_partner', 'lafayette_organization',
                     'community_partner')
+    list_filter = ('status','student_partner', 'faculty_partner', 'lafayette_organization', 'community_partner')
     fields = ('name', 'status', 'student_partner', 'faculty_partner', 'lafayette_organization', 'community_partner',
               'notes')
     search_fields = ('name', 'status', 'student_partner', 'faculty_partner', 'lafayette_organization',
                      'community_partner', 'notes')
+    actions = [mark_upcoming, mark_ongoing, mark_completed]
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'coordinator', 'contact', 'organization', 'date', 'pulse_date', 'num_volunteers')
+    list_filter = ('coordinator', 'organization', 'date', 'pulse_date')
     fields = ('name', 'coordinator', 'contact', 'organization', 'date', 'pulse_date', 'num_volunteers', 'notes')
     search_fields = ('name', 'coordinator', 'contact', 'organization', 'date', 'pulse_date', 'num_volunteers', 'notes')
 
