6f2bc7fb91d10a1d5530ee71e245efdef79cdaee
Michael Baylor <baylor.michael@gmail.com>
2019-11-24 15:27:07 -0500

    Illegal faculty type prevention

--- people/admin.py
+++ people/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django import forms
+
 from .models import Person, Phone, Department, FacultyType
 
 
@@ -10,11 +12,25 @@ class PhoneInLine(admin.StackedInline):
     ordering = ('number',)
 
 
+class PersonForm(forms.ModelForm):
+    model = Person
+
+    class Meta:
+        fields = ('first', 'last', 'email', 'role', 'department', 'faculty_type')
+
+    def clean(self):
+        faculty_type = self.cleaned_data.get("faculty_type")
+        role = self.cleaned_data.get("role")
+
+        if faculty_type and role != "Faculty":
+                raise forms.ValidationError("ERROR: The role must be a faculty for the person to have a faculty type")
+
+
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('first', 'last', 'email', 'role', 'department', 'faculty_type')
-    fields = ('first', 'last', 'email', 'role', 'department', 'faculty_type')
     search_fields = ('first', 'last', 'email', 'role', 'department', 'faculty_type')
     inlines = [PhoneInLine]
+    form = PersonForm
 
 
 class DepartmentAdmin(admin.ModelAdmin):
