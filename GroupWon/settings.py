6ca7864e145b30f2dbd47ae750ad6b3591bd7187
Michael Baylor <baylor.michael@gmail.com>
2019-11-24 17:27:07 -0500

    Switched back to debug

--- GroupWon/settings.py
+++ GroupWon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bl)lvy2d2a$wkz1%jr4#=gk6x4k1o25+3w@4*glq$tuz)226*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
