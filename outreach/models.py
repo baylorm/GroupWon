590cd81cf3d1b166fab1d6d0a98236fe1ea23979
Michael Baylor <baylor.michael@gmail.com>
2019-12-03 15:41:49 -0500

    Added constraints for projects

--- outreach/models.py
+++ outreach/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-
 # Create your models here.
+from django.db.models import Q
+
 from people.models import Person
 
 
 class Organization(models.Model):
     name = models.CharField(max_length=150)
-    type = models.CharField(max_length=150, choices=(('Community Organization', 'Community Organization'), ('Club', 'Club'), ('Course', 'Course')))
+    type = models.CharField(max_length=150, choices=(
+    ('Community Organization', 'Community Organization'), ('Club', 'Club'), ('Course', 'Course')))
     num_members = models.PositiveIntegerField(blank=True, null=True, verbose_name='Number of Members')
     notes = models.TextField(blank=True, null=True)
     contact = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True)
@@ -22,13 +24,17 @@ class Project(models.Model):
                                                        ('Completed', 'Completed')))
     notes = models.TextField(blank=True, null=True)
     community_partner = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True,
-                                          related_name='community', verbose_name='Community Partner')
+                                          related_name='community', verbose_name='Community Partner',
+                                          limit_choices_to={'type': 'Community Organization'})
     lafayette_organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True,
-                                               related_name='lafayette', verbose_name='Lafayette Organization')
+                                               related_name='lafayette', verbose_name='Lafayette Organization',
+                                               limit_choices_to=Q(type='Club') | Q(type='Course'))
     student_partner = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True,
-                                        related_name='student', verbose_name='Student Partner')
+                                        related_name='student', verbose_name='Student Partner',
+                                        limit_choices_to={'role': 'Student'})
     faculty_partner = models.ForeignKey(Person, on_delete=models.SET_NULL, blank=True, null=True,
-                                        related_name='faculty', verbose_name='Faculty Partner')
+                                        related_name='faculty', verbose_name='Faculty Partner',
+                                        limit_choices_to={'role': 'Faculty'})
 
     def __str__(self):
         return self.name
