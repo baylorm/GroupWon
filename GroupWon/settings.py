bd728682f371d8dfa5bce686ebbad1f954589e14
Michael Baylor <baylor.michael@gmail.com>
2019-12-09 20:53:09 -0500

    Final cleanup

--- GroupWon/settings.py
+++ GroupWon/settings.py
@@ -79,24 +79,17 @@ WSGI_APPLICATION = 'GroupWon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'baylorm',
-            'USER': 'baylorm',
-            'PASSWORD': 'EnRajip8',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
+}
 
 
 # Password validation
